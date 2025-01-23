9c1a2bf45ef14c52b3ff2846524244c5cffb8f90
sflugum <siclops.ch@gmail.com>
2025-01-23 10:58:29 -0500

    Added menu option 3

--- mysql-java-projects/src/main/java/projects/ProjectsApp.java
+++ mysql-java-projects/src/main/java/projects/ProjectsApp.java
@@ -13,11 +13,13 @@ public class ProjectsApp {
 
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
-	
+	private Project curProject;
 	
 	// @formatter: off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 	);				
 	// @formatter: on
 			
@@ -38,19 +40,50 @@ public class ProjectsApp {
 				case -1:
 					done = exitMenu();
 					break;
+				
 				case 1:
 					createProject();
 					break;
 					
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Please try again.");
+				case 2:
+					listProjects();
+					break;
 					
+				case 3:
+					selectProject();
+					break;
 				}
 			}
 			catch(Exception e) {
-				System.out.println("\nError: " + e + "Try again.");
+				System.out.println("\nError: " + e + "Try again.");				
 			}
 		}		
+	}
+
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nInvalid project ID selected");
+		}
+		
+	}
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println(" " + project.getProjectId() + ": " 
+				+ project.getProjectName()));
+		
 	}
 
 	private void createProject() {
@@ -130,6 +163,12 @@ public class ProjectsApp {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
 		
 		operations.forEach(line -> System.out.println("  " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		} else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
 	}
 
 }
--- mysql-java-projects/target/classes/projects-schema.sql
+++ mysql-java-projects/target/classes/projects-schema.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (
-	project_id INT AUTO_INCREMENT NOT NULL,
+	project_id INT NOT NULL AUTO_INCREMENT,
 	project_name VARCHAR(64) NOT NULL,
-	estimated_hours INT NOT NULL,
-	actual_hours INT NOT NULL,
-	difficulty VARCHAR(16) NOT NULL,
+	estimated_hours DECIMAL (7,2),
+	actual_hours DECIMAL (7,2),
+	difficulty INT,
 	notes TEXT,
 	PRIMARY KEY (project_id) 
 	
 );
 
 CREATE TABLE category (
-	category_id INT AUTO_INCREMENT NOT NULL,
+	category_id INT NOT NULL AUTO_INCREMENT,
 	category_name VARCHAR (64) NOT NULL,
 	PRIMARY KEY (category_id)
 );
@@ -29,7 +29,7 @@ CREATE TABLE project_category (
 );
 
 CREATE TABLE step (
-	step_id INT AUTO_INCREMENT NOT NULL,
+	step_id INT NOT NULL AUTO_INCREMENT,
 	project_id INT NOT NULL,
 	step_text TEXT,
 	step_order INT NOT NULL,
@@ -38,11 +38,11 @@ CREATE TABLE step (
 );
 
 CREATE TABLE material (
-	material_id INT AUTO_INCREMENT NOT NULL,
+	material_id INT NOT NULL AUTO_INCREMENT,
 	project_id INT NOT NULL,
 	material_name VARCHAR(64) NOT NULL,
 	num_required INT NOT NULL,
-	cost INT NOT NULL,
+	cost INT,
 	PRIMARY KEY (material_id),
 	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE	
 );
